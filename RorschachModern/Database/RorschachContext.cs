2d0d65a392b1de5bd6935ca94504fdf4818e26a2
Sean Tronsen <60711680+seantronsen@users.noreply.github.com>
2022-05-09 13:34:10 -0400

    Updated RorschachContext.cs
    Added a note in the file that discusses the potential security issue that results from directly exposing the database connection string in a public repository.

--- RorschachModern/Database/RorschachContext.cs
+++ RorschachModern/Database/RorschachContext.cs
@@ -17,6 +17,11 @@ namespace RorschachModern.Database
         public DbSet<Question> Questions { get; set; }
         protected override void OnConfiguring( DbContextOptionsBuilder options )
         {
+            /*
+            Please note that this server is no longer active. Additionally, this project was developed early on in my programming carreer, so please 
+            excuse the verbatim connection string (a bit of a security issue) that exists where ENV variables should have been used.
+            -- Sean Tronsen
+            */
             options.UseSqlServer("Server=tcp:tronsen-development.database.windows.net,1433;Initial Catalog=rorschach;Persist Security Info=False;" +
                                  "User ID=phantom_root;Password=Scheise125;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;");
         }
